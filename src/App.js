c1710d70b7e654b8f2b2bc27883765d22d824a45
DesignerSamadhan <36732154+DesignerSamadhan@users.noreply.github.com>
2021-08-20 16:52:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Contacts from './contacts_module/Contacts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
